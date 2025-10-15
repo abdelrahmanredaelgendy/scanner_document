2c715e233b22e7f7621e4a435842bddded2e2bd9
boda <abdelrahmanreda748@gmail.com>
2025-10-15 19:23:26 +0300

    Fix: Update import paths and dependencies for DocumentScannerPlugin.

--- ios/Classes/DocumentScannerPlugin.m
+++ ios/Classes/DocumentScannerPlugin.m
@@ -1,11 +1,11 @@
 #import "DocumentScannerPlugin.h"
-#if __has_include(<document_scanner/document_scanner-Swift.h>)
-#import <document_scanner/document_scanner-Swift.h>
+#if __has_include(<scanner_document/scanner_document-Swift.h>)
+#import <scanner_document/scanner_document-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "document_scanner-Swift.h"
+#import "scanner_document-Swift.h"
 #endif
 
 @implementation DocumentScannerPlugin
